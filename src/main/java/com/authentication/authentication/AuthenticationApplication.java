fac61c1b6da6afe950cf79434ec114827eee4c6d
EstebanGC <johan.gal.car@gmail.com>
2024-04-23 20:02:59 -0500

    Setting config files

--- src/main/java/com/authentication/authentication/AuthenticationApplication.java
+++ src/main/java/com/authentication/authentication/AuthenticationApplication.java
@@ -2,12 +2,11 @@ package com.authentication.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 import java.util.Arrays;
 
 @SpringBootApplication
-@EnableEurekaServer
 public class AuthenticationApplication {
 
 	public static void main(String[] args) {
