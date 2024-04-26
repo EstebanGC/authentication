8c64973c7137c1577d5da78513d1074b2fdff502
EstebanGC <johan.gal.car@gmail.com>
2024-04-25 21:17:47 -0500

    .

--- src/main/java/com/authentication/authentication/config/AuthorizationServerConfiguration.java
+++ src/main/java/com/authentication/authentication/config/AuthorizationServerConfiguration.java
@@ -2,15 +2,20 @@ package com.authentication.authentication.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.oauth2.server.authorization.InMemoryOAuth2AuthorizationConsentService;
 import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsentService;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+
+import javax.sql.DataSource;
 
 
 @Configuration
-public class AuthorizationServerConfiguration extends WebSecurityConfiguration {
+@Import(OAuth2AuthorizationServerConfiguration.class)
+public class AuthorizationServerConfiguration  {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // Configure your application security here (optional)
